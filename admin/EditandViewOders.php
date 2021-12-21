2b8f57880d0c87d597b92fd10c08a53e9e97b956
Dai <trantuandai2508@gmail.com>
2021-12-22 04:50:00 +0700

    Them file EditandViewOders

---
+++ admin/EditandViewOders.php
@@ -0,0 +1,64 @@
+<!-- View modal-->
+<div class="modal fade bd-example-modal-lg" id="view" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-center" id="myModalLabel">Thông tin sản phẩm</h4>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">ID:</span>
+                        <p id="voderID" class="input-group-addon" style="width:150px;"></p>
+                    </div>
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">Mã vận đơn:</span>
+                        <p id="voderVD" class="input-group-addon" style="width:150px;"></p>
+                    </div>
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">Trạng thái:</span>
+                        <p id="voderStatus" class="input-group-addon" style="width:150px;"></p>
+                    </div>   
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">Tổng tiền:</span>
+                        <p id="voderMoney" class="input-group-addon" style="width:150px;"></p>
+                    </div>   
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">Địa chỉ giao:</span>
+                        <p id="voderAddr" class="input-group-addon" style="width:150px;"></p>
+                    </div>  
+                    <div class="form-group input-group">
+                        <span class="input-group-addon" style="width:150px;">Người đặt:</span>
+                        <p id="voderOwner" class="input-group-addon" style="width:150px;"></p>
+                    </div>
+                    <div>
+                        <table style="width:100%">
+                            <tr>
+                                <th>Company</th>
+                                <th>Contact</th>
+                                <th>Country</th>
+                            </tr>
+                            <tr>
+                                <td>Alfreds Futterkiste</td>
+                                <td>Maria Anders</td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td>Centro comercial Moctezuma</td>
+                                <td>Francisco Chang</td>
+                                <td>Mexico</td>
+                            </tr>
+                        </table>
+                    </div>
+
+                </div>  
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span>Đóng</button>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
