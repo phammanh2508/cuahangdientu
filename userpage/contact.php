ef6f843633d79fd368a40157576d3a049740f92a
Manh <phammanh2508@gmail.com>
2021-12-22 00:11:32 +0700

    them file contact

---
+++ userpage/contact.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from template.hasthemes.com/antomi/antomi/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 10:28:47 GMT -->
+<head>
+    <?php include "layout/header.php"; ?>
+</head>
+
+<body>
+
+    <!--Offcanvas menu area start-->
+    <?php include 'layout/menu-mobile.php'; ?>
+    <!--Offcanvas menu area end-->
+
+    <!--header area start-->
+    <?php include "layout/menu-header.php"; ?>
+    <!--header area end-->
+
+    <!--breadcrumbs area start-->
+    <div class="breadcrumbs_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb_content">
+                        <ul>
+                            <li><a href="index.html">home</a></li>
+                            <li>contact us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--breadcrumbs area end-->
+
+    <div class="contact_page_bg">
+        <!--contact map start-->
+        <div class="contact_map">
+            <div class="map-area">
+                <div id="googleMap">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.9543420064997!2d106.67564341402372!3d10.738002462839844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f62a90e5dbd%3A0x674d5126513db295!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBDw7RuZyBOZ2jhu4cgU8OgaSBHw7Ju!5e0!3m2!1svi!2s!4v1636734219421!5m2!1svi!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+        
+        <!--contact map end-->
+        <div class="container">
+            <!--contact area start-->
+            <div class="contact_area">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="contact_message content">
+                            <h3>contact us</h3>
+                            <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram anteposuerit litterarum formas human. qui sequitur mutationem consuetudium lectorum. Mirum est notare quam</p>
+                            <ul>
+                                <li><i class="fa fa-fax"></i> Address : 180 Đường Cao Lỗ, Phường 4, Quận 8, Thành phố Hồ Chí Minh, Việt Nam</li>
+                                <li><i class="fa fa-phone"></i> <a href="#">dh51805934@student.stu.edu.vn</a></li>
+                                <li><i class="fa fa-envelope-o"></i> 0123456789</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="contact_message form">
+                            <h3>Tell us your project</h3>
+                            <form id="contact-form" method="POST" action="https://template.hasthemes.com/antomi/antomi/assets/mail.php">
+                                <p>
+                                    <label> Your Name (required)</label>
+                                    <input name="name" placeholder="Name *" type="text">
+                                </p>
+                                <p>
+                                    <label> Your Email (required)</label>
+                                    <input name="email" placeholder="Email *" type="email">
+                                </p>
+                                <p>
+                                    <label> Subject</label>
+                                    <input name="subject" placeholder="Subject *" type="text">
+                                </p>
+                                <div class="contact_textarea">
+                                    <label> Your Message</label>
+                                    <textarea placeholder="Message *" name="message" class="form-control2"></textarea>
+                                </div>
+                                <button type="submit"> Send</button>
+                                <p class="form-messege"></p>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--contact area end-->
+        </div>
+    </div>
+
+    <!--footer area start-->
+    <?php include "layout/footer.php"; ?>
+
+    <!-- JS
+============================================ -->
+
+
+    <!--map js code here-->
+
+
+</body>
+
+
+<!-- Mirrored from template.hasthemes.com/antomi/antomi/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 10:28:48 GMT -->
+</html>
