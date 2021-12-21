60d0ac4781315c2c2ecb5aa4019e3e202b5f3cc9
Manh <phammanh2508@gmail.com>
2021-12-22 00:21:15 +0700

    them file menu-mobile

---
+++ userpage/layout/menu-mobile.php
@@ -0,0 +1,88 @@
+<div class="off_canvars_overlay"></div>
+<div class="Offcanvas_menu">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="canvas_open">
+                    <a href="javascript:void(0)"><i class="ion-navicon"></i></a>
+                </div>
+                <div class="Offcanvas_menu_wrapper">
+                    <div class="canvas_close">
+                        <a href="javascript:void(0)"><i class="ion-android-close"></i></a>
+                    </div>
+                    <div class="search_container">
+                        <form action="#">
+                            <div class="hover_category">
+                                <select class="select_option" name="select" id="categori1">
+                                    <option selected value="1">Danh mục sản phẩm</option>
+                                    <option value="2">Accessories</option>
+                                    <option value="3">Accessories & More</option>
+                                    <option value="4">Butters & Eggs</option>
+                                    <option value="5">Camera & Video </option>
+                                    <option value="6">Mornitors</option>
+                                    <option value="7">Tablets</option>
+                                    <option value="8">Laptops</option>
+                                    <option value="9">Handbags</option>
+                                    <option value="10">Headphone & Speaker</option>
+                                    <option value="11">Herbs & botanicals</option>
+                                    <option value="12">Vegetables</option>
+                                    <option value="13">Shop</option>
+                                    <option value="14">Laptops & Desktops</option>
+                                    <option value="15">Watchs</option>
+                                    <option value="16">Electronic</option>
+                                </select>
+                            </div>
+                            <div class="search_box">
+                                <input placeholder="Tìm kiếm sản phẩm..." type="text">
+                                <button type="submit">Tìm kiếm</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="menu" class="text-left ">
+                        <ul class="offcanvas_main_menu">
+                            <li class="menu-item-has-children active">
+                                <a href="index.php">Trang chủ</a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="shop.php">Shop</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item-has-children">
+                                        <a href="#">other Pages</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="cart.php">cart</a></li>
+                                            <li><a href="wishlist.php">Wishlist</a></li>
+                                            <li><a href="checkout.php">Checkout</a></li>
+                                            <li><a href="my-account.php">my account</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children">
+                                        <a href="product-sidebar.php">Product Types</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="login.php">login </a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="about.php">About Us</a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="contact.php"> Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="Offcanvas_footer">
+                        <span><a href="#"><i class="fa fa-envelope-o"></i> demo@example.com</a></span>
+                        <ul>
+                            <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li class="pinterest"><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                            <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li class="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
